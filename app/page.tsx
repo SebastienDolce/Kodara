50edb9ed0097b56a63e81abb6ae5d152302e9e77
Sebastien Dolce <sebastiendolce@gmail.com>
2025-06-21 13:41:01 -0400

    bug

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,17 @@
 import KodaraServer from './components/KodaraServer';
 
 export default function Page() {
-  return <KodaraServer />;
+  return <><KodaraServer />
+  <form name="contact" data-netlify="true" data-netlify-honeypot="bot-field" hidden>
+    <input type="text" name="name" />
+    <input type="email" name="email" />
+    <select name="project_type">
+      <option value="General Inquiry">General Inquiry</option>
+      <option value="Project Quote">Project Quote</option>
+      <option value="Collaboration">Collaboration</option>
+      <option value="Feedback">Feedback</option>
+    </select>
+    <textarea name="message"></textarea>
+  </form></>
+;
 }
-<form name="contact" data-netlify="true" data-netlify-honeypot="bot-field" hidden>
-  <input type="text" name="name" />
-  <input type="email" name="email" />
-  <select name="project_type">
-    <option value="General Inquiry">General Inquiry</option>
-    <option value="Project Quote">Project Quote</option>
-    <option value="Collaboration">Collaboration</option>
-    <option value="Feedback">Feedback</option>
-  </select>
-  <textarea name="message"></textarea>
-</form>
